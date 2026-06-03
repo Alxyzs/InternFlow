65120056b8af5fc4b18d168bea99d03708e1294d
AliHasanov16 <ayxanazevov@gmail.com>
2026-06-03 15:04:09 +0300

    Fixed ActivityLog to show user and task names instead of IDs

--- InternFlow.MVC/Controllers/ActivityLogController.cs
+++ InternFlow.MVC/Controllers/ActivityLogController.cs
@@ -8,15 +8,25 @@ namespace InternFlow.MVC.Controllers
     public class ActivityLogController : Controller
     {
         private readonly IActivityLogService _activityLogService;
+        private readonly IUserService _userService;
+        private readonly ITaskService _taskService;
 
-        public ActivityLogController(IActivityLogService activityLogService)
+        public ActivityLogController(IActivityLogService activityLogService, IUserService userService, ITaskService taskService)
         {
             _activityLogService = activityLogService;
+            _userService = userService;
+            _taskService = taskService;
         }
 
         public IActionResult Index()
         {
             var logs = _activityLogService.GetAll();
+            var users = _userService.GetAll();
+            var tasks = _taskService.GetAll();
+
+            ViewBag.Users = users;
+            ViewBag.Tasks = tasks;
+
             return View(logs);
         }
     }
